91a92de2c3ec54469aad990c6169ff25006296b5
Despagino <gtasis84@gmail.com>
2021-06-14 15:55:49 -0400

    commit

---
+++ src/com/OOPConcepts/Main.java
@@ -0,0 +1,9 @@
+package com.OOPConcepts;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/com/OOPConcepts/PersonInterface.java
@@ -0,0 +1,12 @@
+package com.OOPConcepts;
+
+public interface PersonInterface {
+
+    static final String ORGINIZATION = "Per Scholar";
+
+    double calculate (int num);
+
+    void checker ();
+}
+
+
